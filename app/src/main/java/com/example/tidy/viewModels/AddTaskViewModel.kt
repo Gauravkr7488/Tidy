b9b4e8dcb88dfa2eefc47bfac3d954bf4dc5febd
Gaurav <gauravkr7488@gmail.com>
2026-03-07 22:26:33 +0530

    created AddTaskViewModel.kt

--- app/src/main/java/com/example/tidy/viewModels/AddTaskViewModel.kt
+++ app/src/main/java/com/example/tidy/viewModels/AddTaskViewModel.kt
@@ -1,5 +1,31 @@
 package com.example.tidy.viewModels
 
 class AddTaskViewModel {
+    private var flag: String = ""
+    private var taskId: Long? = null
+
+    fun getFlag(): String {
+        val f = flag
+        flag = ""
+        return f
+    }
+
+    fun setId(id: Long){
+        taskId = id
+    }
+
+    fun getId(): Long? {
+        val id = taskId
+        taskId = null
+        return id
+    }
+
+    fun setChildFlag(){
+        flag = "child"
+    }
+
+    fun setParentFlag(){
+        flag = "parent"
+    }
 
 }
