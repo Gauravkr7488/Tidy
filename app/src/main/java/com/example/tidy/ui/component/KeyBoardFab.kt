3d9870924c51ec1152f95619873c15c93d3ac24b
Gaurav <gauravkr7488@gmail.com>
2026-03-13 09:41:28 +0530

    fixed the save button moving problem

--- app/src/main/java/com/example/tidy/ui/component/KeyBoardFab.kt
+++ app/src/main/java/com/example/tidy/ui/component/KeyBoardFab.kt
@@ -31,21 +31,33 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.filled.Save
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 @Composable
 fun KeyboardAwareFAB(
     onClick: () -> Unit
 ) {
     val imePadding = WindowInsets.ime.asPaddingValues().calculateBottomPadding()
-
+    var isVisible by remember { mutableStateOf(true) }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.BottomEnd
     ) {
-        FloatingActionButton(
-            onClick = onClick,
-            modifier = Modifier.padding(end = 16.dp, bottom = imePadding + 16.dp).size(80.dp) // 16dp above keyboard
-        ) {
-            Icon(Icons.Filled.Save, contentDescription = "Save Task")
+        if (isVisible) {
+            FloatingActionButton(
+                onClick = {
+                    isVisible = false
+                    onClick()
+                },
+                modifier = Modifier
+                    .padding(end = 16.dp, bottom = imePadding + 16.dp)
+                    .size(80.dp) // 16dp above keyboard
+            ) {
+                Icon(Icons.Filled.Save, contentDescription = "Save Task")
+            }
         }
     }
 }
