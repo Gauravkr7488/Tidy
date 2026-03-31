91e8ce5c0542e723171f02b1735adb4bc2fe9104
Gaurav <gauravkr7488@gmail.com>
2026-03-31 12:38:27 +0530

    created HomeScreenViewModel.kt

---
+++ app/src/main/java/com/example/tidy/viewModels/HomeScreenViewModel.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2026  Gaurav Kumar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.example.tidy.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import com.example.tidy.DbOperation
+import com.example.tidy.Task
+import com.example.tidy.constants.Routes
+import kotlinx.coroutines.launch
+
+class HomeScreenViewModel(
+    private val dbOperation: DbOperation,
+    private val addTaskViewModel: AddTaskViewModel,
+    private val navController: NavController,
+) : ViewModel() {
+    fun toggleDoneStatus(task: Task) {
+        viewModelScope.launch {
+            dbOperation.updateDoneStatus(task)
+        }
+    }
+
+    fun editTask(task: Task) {
+        addTaskViewModel.setCurrentTaskId(task.id)
+        navController.navigate(Routes.ADD_TASK)
+    }
+
+    fun skipTask(task: Task){
+        viewModelScope.launch {
+            dbOperation.skipTask(task)
+        }
+    }
+
+    fun deleteTask(task: Task){
+        viewModelScope.launch {
+            dbOperation.deleteTask(task)
+        }
+    }
+}
