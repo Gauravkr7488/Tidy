1eaab441d75c9e32cd4ee02f10c007b496d83761
Gaurav <gauravkr7488@gmail.com>
2026-03-31 11:03:34 +0530

    modified TaskRow.kt

--- app/src/main/java/com/example/tidy/ui/component/TaskRow.kt
+++ app/src/main/java/com/example/tidy/ui/component/TaskRow.kt
@@ -20,10 +20,16 @@ package com.example.tidy.ui.component
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -32,22 +38,42 @@ import androidx.compose.ui.unit.dp
 fun TaskRow(
     title: String,
     doneStatus: Boolean,
-    onLongTap: () -> Unit,
+    showMenu: Boolean,
+    onCloseMenu: () -> Unit,
+    onOpenMenu: () -> Unit,
+    menuContent: @Composable () -> Unit,
     modifier: Modifier = Modifier,
-    onTap: () -> Unit = {},
 ) {
-    Text(
-        text = title,
-        style = MaterialTheme.typography.bodyLarge,
-        textDecoration = if (doneStatus) TextDecoration.LineThrough else TextDecoration.None,
+    var tapOffset by remember { mutableStateOf(Offset.Zero) }
+
+    Card(
         modifier = modifier
-            .padding(start = 8.dp)
-            .fillMaxWidth()
             .pointerInput(Unit) {
                 detectTapGestures(
-                    onLongPress = { onLongTap() },
-                    onTap = { onTap() }
+                    onLongPress = { offset ->
+                        tapOffset = offset
+                        onOpenMenu()
+                    },
                 )
             }
-    )
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodyLarge,
+            textDecoration = if (doneStatus) TextDecoration.LineThrough else TextDecoration.None,
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .fillMaxWidth()
+
+        )
+        TaskRowMenu(
+            showMenu = showMenu,
+            tapOffset = tapOffset,
+            onDismiss = {
+                onCloseMenu()
+            }
+        ) {
+            menuContent()
+        }
+    }
 }
