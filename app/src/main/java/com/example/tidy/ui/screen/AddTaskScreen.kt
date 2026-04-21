3014ffd4d25902b8e8ce7d8f740b37cfd7ee919c
Gaurav <gauravkr7488@gmail.com>
2026-04-21 07:43:33 +0530

    added daily as repeat default

--- app/src/main/java/com/example/tidy/ui/screen/AddTaskScreen.kt
+++ app/src/main/java/com/example/tidy/ui/screen/AddTaskScreen.kt
@@ -217,8 +217,9 @@ fun AddTaskScreen(
                     Spacer(modifier = Modifier.weight(1f))
                     Button(
                         onClick = {
+                            repeatType =
+                                if (repeatType != RepeatTypes.NONE) RepeatTypes.NONE else RepeatTypes.DAILY
                             repeatStatus = !repeatStatus
-                            repeatType = RepeatTypes.NONE
                         }
                     ) {
                         Icon(
