464687c53d8f7297e56f5be36ef5ca1c8393a94a
Gaurav <gauravkr7488@gmail.com>
2026-07-05 11:04:20 +0530

    cleanup

--- app/src/main/java/com/example/tidy/ui/component/buttons/OutlinedDropDownButton.kt
+++ app/src/main/java/com/example/tidy/ui/component/buttons/OutlinedDropDownButton.kt
@@ -16,22 +16,11 @@
  */
 package com.example.tidy.ui.component.buttons
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.tidy.ui.component.menu.OutlinedMenuItem
 
 
