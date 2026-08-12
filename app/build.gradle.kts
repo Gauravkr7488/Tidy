7633a688c91f3938aae042d14b06e8d7e7322062
Gaurav <gauravkr7488@gmail.com>
2026-08-12 08:59:23 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 88
-        versionName = "3.38.49"
+        versionCode = 89
+        versionName = "3.38.50"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
