3130a462eff7d422617f6331469f1d635cf9c08a
Gaurav <gauravkr7488@gmail.com>
2026-09-15 14:58:45 +0530

    archive issue fixed

--- app/src/main/java/com/example/tidy/viewModels/SharedViewModel.kt
+++ app/src/main/java/com/example/tidy/viewModels/SharedViewModel.kt
@@ -151,11 +151,15 @@ class SharedViewModel(
 
     private suspend fun syncTaskWithParent(task: Task): Task {
         if (task.parentId == null) return task
-        val emptyTask = Utils.getEmptyTask()
         val parentTask = taskService.getTask(task.parentId)
         if (parentTask.done != task.done) {
-            syncParentAndChildDoneStats(parentTask)
+            resolveDoneStat(parentTask)
         }
+        if (parentTask.hide != task.hide) {
+            val l = getDescendantList(task)
+            l.forEach { taskService.saveTask(it.copy(hide = parentTask.hide)) }
+        }
+        val emptyTask = Utils.getEmptyTask()
         return task.copy(
             hide = parentTask.hide,
             repeatType = emptyTask.repeatType,
@@ -168,7 +172,7 @@ class SharedViewModel(
         )
     }
 
-    private suspend fun syncParentAndChildDoneStats(parentTask: Task) {
+    private suspend fun resolveDoneStat(parentTask: Task) {
         if (parentTask.done) {
             val updatedParent = parentTask.copy(done = false)
             saveTask(updatedParent)
